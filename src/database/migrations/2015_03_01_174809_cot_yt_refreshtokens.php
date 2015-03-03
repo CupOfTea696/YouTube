c581fbb3269a5efa4117837e1135acf154d9ae45
CupOfTea696 <cupoftea696@gmail.com>
2015-03-03 22:59:55 +0000

    migration fix

--- src/database/migrations/2015_03_01_174809_cot_yt_refreshtokens.php
+++ src/database/migrations/2015_03_01_174809_cot_yt_refreshtokens.php
@@ -6,14 +6,17 @@ use Illuminate\Database\Migrations\Migration;
 
 class CotYtRefreshtokens extends Migration {
     
-    protected $table;
-    protected $model;
+    protected $tokenTable;
+    protected $userTable;
     protected $userKey;
     
     public function __construct(){
-        $this->table = Config::get('youtube::youtube.table');
-        $this->model = Config::get('auth.model');
-        $this->userKey = $this->model::getKeyName();
+        $userModel = Config::get('auth.model');
+        $user = new $userModel;
+        
+        $this->tokenTable = Config::get('youtube.table');
+        $this->userTable = Config::get('auth.table');
+        $this->userKey = $user->getKeyName();
     }
     
 	/**
@@ -23,13 +26,13 @@ class CotYtRefreshtokens extends Migration {
 	 */
 	public function up()
 	{
-        Schema::create($this->table, function(Blueprint $table)
+        Schema::create($this->tokenTable, function(Blueprint $table)
 		{
 			$table->increments('refreshTokenID');
 			$table->string('token', 45);
             $table->integer($this->userKey)->unsigned();
 			$table->foreign($this->userKey)
-                  ->references($this->userKey)->on($this->model)
+                  ->references($this->userKey)->on($this->userTable)
                   ->onDelete('cascade');
 		});
 	}
@@ -41,7 +44,7 @@ class CotYtRefreshtokens extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop($this->table);
+		Schema::drop($this->tokenTable);
 	}
     
 }
