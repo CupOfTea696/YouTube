3de5f61c67ff42f8c2af7b7934107cf9babeb205
CupOfTea696 <cupoftea696@gmail.com>
2015-06-01 15:00:10 +0200

    Release: v0.5.6-beta

--- src/YouTube.php
+++ src/YouTube.php
@@ -16,7 +16,7 @@ class YouTube implements ProviderContract, Serializable {
     use Package;
     
     const PACKAGE = 'CupOfTea/YouTube';
-    const VERSION = '0.5.5-beta';
+    const VERSION = '0.5.6-beta';
     
 	/**
 	 * Available Resources for this API.
